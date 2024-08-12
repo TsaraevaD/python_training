858841b1e7d809acda3dfaec635e5c1993829f80
Diana <artemidadelos@gmail.com>
2024-08-12 23:40:15 +0300

    фикстура для контактов

--- new_contact.py
+++ new_contact.py
@@ -1,60 +1,19 @@
 # -*- coding: utf-8 -*-
-from selenium import webdriver
-import unittest
+import pytest
+from attachment import Attachment
 from contact import Contact
 
-class AddGroup(unittest.TestCase):
-    def setUp(self):
-        self.wd = webdriver.Firefox()
-        self.wd.implicitly_wait(30)
+@pytest.fixture
+def att(request):
+    fixture = Attachment()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
     
-    def test_add_contact(self):
-        wd = self.wd
-        self.open_home_page(wd)
-        self.login(wd, "admin", "secret")
-        self.open_new_contact(wd)
-        self.create_contact(wd, Contact("f-name", "mid-name", "l-name"))
-        self.return_to_contacts(wd)
-        self.logout(wd)
-
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
-
-    def return_to_contacts(self, wd):
-        wd.find_element_by_link_text("home page").click()
-
-    def create_contact(self, wd, contact):
-        # fill contact form
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").clear()
-        wd.find_element_by_name("firstname").send_keys(contact.firstname)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").clear()
-        wd.find_element_by_name("middlename").send_keys(contact.middlename)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").clear()
-        wd.find_element_by_name("lastname").send_keys(contact.lastname)
-        # submit
-        wd.find_element_by_xpath("//div[@id='content']/form/input[20]").click()
-
-    def open_new_contact(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, username, password):
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(username)
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-
-    def open_home_page(self, wd):
-        wd.get("http://localhost/addressbook/index.php")
-
-    def tearDown(self):
-        self.wd.quit()
-
-if __name__ == "__main__":
-    unittest.main()
+def test_add_contact(att):
+    att.open_home_page()
+    att.login("admin", "secret")
+    att.open_new_contact()
+    att.create_contact(Contact("f-name", "mid-name", "l-name"))
+    att.return_to_contacts()
+    att.logout()
