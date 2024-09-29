51f2d8cc571585d907706b6aa53d43bb4eee61db
Diana <artemidadelos@gmail.com>
2024-09-29 17:43:54 +0300

    выполнена проверка текущей страницы

--- fixture/contact.py
+++ fixture/contact.py
@@ -56,7 +56,8 @@ class ContactHelper:
 
     def open_home_page(self):
         wd = self.app.wd
-        wd.find_element_by_link_text("home").click()
+        if not (wd.current_url.endswith("./") and len(wd.find_elements_by_name("searchform") > 0)):
+            wd.find_element_by_link_text("home").click()
 
 
 
