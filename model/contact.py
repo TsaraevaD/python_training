0ae2c5e7c03462e0bbfbfeea9218a5dfd6a884ef
Diana <artemidadelos@gmail.com>
2024-11-06 18:58:27 +0300

    проверки для контактов и групп

--- model/contact.py
+++ model/contact.py
@@ -1,10 +1,20 @@
 from sys import maxsize
 class Contact:
-    def __init__(self, first_name=None, middle_name=None, last_name=None, id=None):
-        self.firstname = first_name
-        self.middlename = middle_name
-        self.lastname = last_name
+    def __init__(self, firstname=None, middlename=None, lastname=None, id=None, address=None, homephone=None, mobilephone=None,
+                 workphone=None, email=None, email2=None, email3=None, all_phones_from_home_page=None, all_emails_from_home_page=None):
+        self.firstname = firstname
+        self.middlename = middlename
+        self.lastname = lastname
         self.id = id
+        self.homephone = homephone
+        self.mobilephone = mobilephone
+        self.workphone = workphone
+        self.address = address
+        self.email = email
+        self.email2 = email2
+        self.email3 = email3
+        self.all_phones_from_home_page = all_phones_from_home_page
+        self.all_emails_from_home_page = all_emails_from_home_page
 
     def __repr__(self):
         return "%s:%s:%s" % (self.id, self.firstname, self.lastname)
