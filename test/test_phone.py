8a5ca23d9ab785ea090d41e806cec6809c6c33ea
Diana <artemidadelos@gmail.com>
2024-11-08 10:22:45 +0300

    доработан тест для проверки телефонов

--- test/test_phone.py
+++ test/test_phone.py
@@ -4,6 +4,9 @@ def test_phones_on_home_page(app):
     contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.all_emails_from_home_page == merge_emails_like_on_home_page(contact_from_edit_page)
+    assert contact_from_home_page.lastname == contact_from_edit_page.lastname
+    assert contact_from_home_page.firstname == contact_from_edit_page.firstname
+    assert contact_from_home_page.address == contact_from_edit_page.address
 
 def test_phones_on_contact_view_page(app):
     contact_from_view_page = app.contact.get_contact_from_view_page(0)
@@ -23,8 +26,7 @@ def merge_phones_like_on_home_page(contact):
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.email, contact.email2, contact.email3]))))
+                            filter(lambda x: x is not None,
+                                   [contact.email, contact.email2, contact.email3])))
 
 
