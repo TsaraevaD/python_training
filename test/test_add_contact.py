47f75ce965a36b9f814d4f1af57f5bab521d7708
Diana <artemidadelos@gmail.com>
2024-11-10 21:52:24 +0300

    Параметризованы группы и контакты

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,8 +1,23 @@
 from model.contact import Contact
+import pytest
+import random
+import string
+
+def random_string(prefix,maxlen):
+        symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
+        return prefix + "" .join([random.choice(symbols) for i in range(random.randrange(maxlen))])
+
+testdata = [
+        Contact(firstname=firstname, middlename=middlename, lastname=lastname)
+        for firstname in ["", random_string("firstname",10)]
+        for middlename in ["", random_string("middlename",20)]
+        for lastname in ["", random_string("lastname",20)]
+
+]
+@pytest.mark.parametrize("contact", testdata, ids=(repr(x) for x in testdata))
     
-def test_add_contact(app):
+def test_add_contact(app, contact):
     old_contacts = app.contact.get_contact_list()
-    contact = Contact("f-name", "mid-name", "l-name")
     app.contact.add()
     app.contact.create(contact)
     new_contacts = app.contact.get_contact_list()
