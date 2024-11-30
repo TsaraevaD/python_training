b8343756ca012e53bfcc53b5ccaab2201143bb9b
Diana <artemidadelos@gmail.com>
2024-11-30 23:23:08 +0300

    Добавлены генераторы тестовых данных

---
+++ data/contacts.py
@@ -0,0 +1,7 @@
+from model.contact import Contact
+
+
+testdata = [
+    Contact(firstname="firstname", lastname="lastname", homephone="homephone", address="address", email="email"),
+    Contact(firstname="firstname2", lastname="lastname2", homephone="homephone2", address="address2", email="email2")
+]
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -1,22 +1,8 @@
 from model.contact import Contact
-import pytest
-import random
-import string
 
-def random_string(prefix,maxlen):
-        symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
-        return prefix + "" .join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-testdata = [
-        Contact(firstname=firstname, middlename=middlename, lastname=lastname)
-        for firstname in ["", random_string("firstname",10)]
-        for middlename in ["", random_string("middlename",20)]
-        for lastname in ["", random_string("lastname",20)]
-
-]
-@pytest.mark.parametrize("contact", testdata, ids=(repr(x) for x in testdata))
     
-def test_add_contact(app, contact):
+def test_add_contact(app, data_contacts):
+    contact = data_contacts
     old_contacts = app.contact.get_contact_list()
     app.contact.add()
     app.contact.create(contact)
@@ -24,3 +10,4 @@ def test_add_contact(app, contact):
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
+
