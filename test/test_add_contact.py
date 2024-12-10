1bcdce57660c034ae0e7176ed3f76e515ebd5078
Diana <artemidadelos@gmail.com>
2024-12-10 11:39:13 +0300

    Проверка списков

--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -9,6 +9,7 @@ def test_add_contact(app, db, json_contacts, check_ui):
     new_contacts = db.get_contact_list()
     assert len(old_contacts) + 1 == len(new_contacts)
     old_contacts.append(contact)
+    assert sorted(new_contacts, key=Contact.id_or_max) == sorted(old_contacts, key=Contact.id_or_max)
     if check_ui:
         assert sorted(new_contacts, key=Contact.id_or_max) == sorted(app.contact.get_contact_list(), key=Contact.id_or_max)
 
