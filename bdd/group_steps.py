1707dd51cb5abe2f6816080d4533e6ec2dea773a
Diana <artemidadelos@gmail.com>
2025-04-10 15:43:45 +0300

    fix bdd

--- bdd/group_steps.py
+++ bdd/group_steps.py
@@ -69,6 +69,6 @@ def verify_group_modified(db, non_empty_group_list, random_group, app, check_ui,
     old_groups = non_empty_group_list
     new_groups = db.get_group_list()
     new_data.id = random_group.id
-    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+    assert old_groups == new_groups
     if check_ui:
         assert sorted(new_groups, key=Group.id_or_max) == sorted(app.group.get_group_list(), key=Group.id_or_max)
